51d769823b3e0389ec10ed54d15833b5da1f4dd2
azulbid <azulbid@gmail.com>
2023-03-27 11:19:01 -0300

    lista

--- db/index.js
+++ db/index.js
@@ -1,5 +1,46 @@
 products = {
-    lista: [
+    lista: [{
+		id: 1,
+		nombre: "Venus de Milo",
+		Artista: "Alejandro de Antioquía",
+		Técnica: "Escultura de Mármol",
+		Dimensiones: "211 cm de alto",
+        Creación: "130 a. C.-100 a. C.",
+	}, {
+		id: 2,
+		nombre: "Apolo y Dafne",
+		Artista: "Francesco Albani",
+        Técnica: "Óleo sobre cobre fijado en tabla",
+        Dimensiones:"17.5 cm × 35.5 cm",
+		Creación: "entre 1615 y 1620",
+	}, {
+		id: 3,
+		nombre: "Autorretrato",
+		Artista: "Alberto Durero",
+		Técnica: "Escultura de Mármol",
+		Dimensiones: "211 cm de alto",
+        Creación: "130 a. C.-100 a. C.",
+
+	}, {
+		id: 4,
+		nombre: "Autorretrato",
+		Artista: "Alberto Durero",
+		Técnica: "Óleo sobre vitela (pergamino) pegado a una tela",
+		Dimensiones: "56,5 cm × 44,5 cm",
+        Creación: "130 a. C.-100 a. C.",
+
+	}, 
+{
+		id: 4,
+		nombre: "Venus del Pardo",
+		Artista: "Tiziano",
+		Técnica: "Óleo sobre lienzo",
+		Dimensiones: "196 cm × 385 cm.",
+        Creación: "1535-1540",
+
+
+	}, 
+
     ]
 }
 
