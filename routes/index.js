cd9b9134aa76e46c940158740b183bbf70f22262
azulbid <azulbid@gmail.com>
2023-04-05 10:02:03 -0300

    agregue formulario

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+//var registro = express.Registro()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
 module.exports = router;
+
+app.get('/registro', (req, res) => {
+  res.render('register');
+});
+//module.exports = registro; 
