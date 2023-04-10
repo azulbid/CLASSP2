cebdd95f16f988586dc5a9167af2f9f7674d8461
azulbid <azulbid@gmail.com>
2023-04-10 11:13:58 -0300

    search results

--- controllers/searchResultsController.js
+++ controllers/searchResultsController.js
@@ -1,7 +1,7 @@
 let db = require('../db/index')
 let searchResultsController = {
     showRes: function(req,res){
-        res.render('search-results');
+        res.render('search-results', {lista:db.lista});
     }
 }
 module.exports = searchResultsController
