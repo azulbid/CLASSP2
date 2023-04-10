ccb5ff35c66927af53416aeffea7d382ac18ba58
azulbid <azulbid@gmail.com>
2023-04-10 20:29:46 -0300

    cambiando mas comentados

--- db/index.js
+++ db/index.js
@@ -59,51 +59,78 @@ let products = {
 	}
     ],
 
+	comentarios: [{
+		id: 1,
+		user: 'Jano',
+		comment: 'Muy buena obra!',
+	},
+	{
+		id: 2,
+		user: 'Santino',
+		comment: 'WOW!',
+	},
+	{
+		id: 3,
+		user: 'Miranda',
+		comment: 'Sobrevalorada!!!!!!'
+	},
+	{
+		id: 4,
+		user: 'Juli',
+		comment: 'AMOOOOOO!',
+	},
+	{
+		id: 5,
+		user: 'Uma',
+		comment: 'Malisima!!!!!!'
+	}
+],
+
 	
 /* cambiar la info de mascomentados */
-	mascomentados : [ {
+	mascomentados: [ {
 			id: 1,
-			nombre: "cambiar",
-			Artista: "Alejandro de Antioquía",
-			Técnica: "Escultura de Mármol",
-			Dimensiones: "211 cm de alto",
+			nombre: "La creación de Adan",
+			Artista: "Miguel Angel",
+			Técnica: "Acrilico",
+			Dimensiones: "211 cm x 30 cm",
 			Creación: "130 a. C.-100 a. C.",
-			imagen: '/images/venusdemilo.webp',
+			imagen: '',
 		}, {
 			id: 2,
-			nombre: "Apolo y Dafne",
-			Artista: "Francesco Albani",
+			nombre: "Nenúfares",
+			Artista: "Monet",
 			Técnica: "Óleo sobre cobre fijado en tabla",
 			Dimensiones:"17.5 cm × 35.5 cm",
 			Creación: "entre 1615 y 1620",
-			imagen: '/images/apollo_d.jpeg',
+			imagen: '',
 		}, {
 			id: 3,
-			nombre: "La Gioconda o Monna Lisa",
-			Artista: "Leonardo da Vinci",
+			nombre: "La joven de la perla",
+			Artista: "Vermeer",
 			Técnica: "Pintura al óleo sobre tabla de álamo",
 			Dimensiones: "79 cm × 53 cm",
 			Creación: "	1503-1519.",
-			imagen: '/images/lamona.jpeg',
+			imagen: '',
 
 		}, {
 			id: 4,
-			nombre: "Autorretrato",
-			Artista: "Alberto Durero",
+			nombre: "La noche estrellada",
+			Artista: "Vincent van Gogh",
 			Técnica: "Óleo sobre vitela (pergamino) pegado a una tela",
 			Dimensiones: "56,5 cm × 44,5 cm",
 			Creación: "130 a. C.-100 a. C.",
-			imagen: '/images/autorretrato.jpg',
+			imagen: '',
 
 		}, 
 	{
 			id: 5,
-			nombre: "Venus del Pardo",
-			Artista: "Tiziano",
+			nombre: "El beso",
+			Artista: "Klimt",
 			Técnica: "Óleo sobre lienzo",
 			Dimensiones: "196 cm × 385 cm.",
 			Creación: "1535-1540",
-			imagen: '/images/venusdelpardo.jpg',
+			imagen: '',
 
 		}
 	],
