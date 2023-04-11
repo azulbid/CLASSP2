e5882aba5cd31bdacbbe6905c5dbebaefcad6acf
azulbid <azulbid@gmail.com>
2023-04-10 21:23:41 -0300

    AAAAA no anda el for

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,7 @@
 // MAIN
 
 let db = require('../db/index.js')
+let db2 = require('../db/index2.js')
 
 let indexController = {
     index: function(req,res){
@@ -8,6 +9,11 @@ let indexController = {
             lista: db.products,
           //  mascomentadas: db.masco,
           //  comentarios: db.comments,
+        })
+    },
+    index2: function(req,res){
+        res.render('index2',{
+            comment: db2.masco,
         })
     }
 }
