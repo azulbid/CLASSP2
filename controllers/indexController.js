4258595dfc5b4486dd68f366fa3ec66611713899
nteller8 <nteller@udesa.edu.ar>
2023-04-10 21:20:55 -0300

    cambios

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -5,9 +5,7 @@ let db = require('../db/index.js')
 let indexController = {
     index: function(req,res){
         res.render('index',{
-            lista: db.products,
-          //  mascomentadas: db.masco,
-          //  comentarios: db.comments,
+            lista: db.lista
         })
     }
 }
