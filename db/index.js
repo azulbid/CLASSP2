bc3e36a96f3beffca2ab16e97f279d9cafd4d41f
azulbid <azulbid@gmail.com>
2023-04-10 22:59:46 -0300

    arregle algo de data

--- db/index.js
+++ db/index.js
@@ -1,7 +1,6 @@
 let products = {
 	usuario: [
 		{
-
 			id: 1,
 			nombreUsuario: 'NinuchaDeSanlorenzo',
 			clave: 'Vivone`',
@@ -58,53 +57,7 @@ let products = {
 		Creación: "1535-1540",
 		imagen: '/images/venusdelpardo.jpeg',
 		comentarios: 'Muy buena!'
-	},
-	{
-		id: 1,
-		nombre: "La creación de Adan",
-		Artista: "Miguel Angel",
-		Técnica: "Acrilico",
-		Dimensiones: "211 cm x 30 cm",
-		Creación: "130 a. C.-100 a. C.",
-		imagen: '/images/adan.png',
-	}, {
-		id: 2,
-		nombre: "Nenúfares",
-		Artista: "Monet",
-		Técnica: "Óleo sobre cobre fijado en tabla",
-		Dimensiones: "17.5 cm × 35.5 cm",
-		Creación: "entre 1615 y 1620",
-		imagen: '/images/nenufares.png',
-	}, {
-		id: 3,
-		nombre: "La joven de la perla",
-		Artista: "Vermeer",
-		Técnica: "Pintura al óleo sobre tabla de álamo",
-		Dimensiones: "79 cm × 53 cm",
-		Creación: "	1503-1519.",
-		imagen: '/images/lajoven.png',
-
-	}, {
-		id: 4,
-		nombre: "La noche estrellada",
-		Artista: "Vincent van Gogh",
-		Técnica: "Óleo sobre vitela (pergamino) pegado a una tela",
-		Dimensiones: "56,5 cm × 44,5 cm",
-		Creación: "130 a. C.-100 a. C.",
-		imagen: '/images/lanocheestrellada.png',
-
-	},
-	{
-		id: 5,
-		nombre: "El beso",
-		Artista: "Klimt",
-		Técnica: "Óleo sobre lienzo",
-		Dimensiones: "196 cm × 385 cm.",
-		Creación: "1535-1540",
-		imagen: '/images/elbeso.png',
-
-	}
-	],
+	}],
 
 	comment: [ {
 		id: 1,
