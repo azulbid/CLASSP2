9de0761cdf6a23afb6b08db28d313b06b7af1331
azulbid <azulbid@gmail.com>
2023-04-12 09:47:11 -0300

    ninucha ok

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -4,7 +4,8 @@ let usu = require('../db/usuario')
 let profileController = {
     det: function(req,res){
         return res.render('profile', {
-            datosUsuario: db.usuario, lista: db.lista, comentarios: db.comentarios 
+            datosUsuario: db.usuario, lista: db.lista, comentarios: db.comentarios,
+             usuario: usu.nombreUsuario
         })
     },
     edit: function(req,res){
@@ -12,11 +13,7 @@ let profileController = {
             datosUsuario: db.usuario
         })
     },
-    usuario: function(req,res){
-        return res.render('usuario',{
-            usuario: usu.usuario
-        })
-    },
+
 
    // profile: function(req,res){
      //   return res.render('profile', {nombre: usu[0].nombre, email: usu[0].email, dni:usu[0].dni, cumple: usu[0].fechaNac, clave:db[0].clave })
