de490677d6a39481caab464fc0ee2963200f706e
nteller8 <nteller@udesa.edu.ar>
2023-05-22 12:22:21 -0300

    cambios register

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,8 +1,39 @@
 //REGISTRO
+let db = require('../database/models')
+
 let registerController = {
     register: function(req, res){
         res.render('register');
+    },
+
+
+register: function(req, res){
+    return res.render('register');
+},
+store: function(req, res){
+    let form = req.body
+
+    //Encriptar la contraseña antes de guardar en la base de datos.
+    let user = {
+        email:form.email,
+        name: form.username,
+        password: form.password,
     }
-}
+
+    //Usar un método de Sequelize para guardar datos.
+    db.User.create(user) //Pasar un objeto literal con los datos a guardar.
+        .then(function(usuarioCreado){ //retorna el elemento creado
+            //Dentro del then debería redireccionar a otra ruta.
+            console.log(usuarioCreado);
+                // return res.send(form);
+            return res.redirect('/movies');
+        })
+        .catch(function(e){
+            console.log(e);
+        })
+
+
+
+}}
 
 module.exports = registerController;
--- routes/register.js
+++ routes/register.js
@@ -2,5 +2,9 @@ var express = require('express');
 var router = express.Router();
 let registerController = require('../controllers/registerController')
 
+
+router.get('/register',indexController.register); //Mostrar el form de registro.
+router.post('/register', indexController.store); //Guarda al usuario en la base de datos.
+
 router.get('/', registerController.register);
 module.exports = router;
