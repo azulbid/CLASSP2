3e09fedbb99c384446b7c612d139ce6f7a10311d
Lorenzo Cozzo <lcozzo@udesa.edu.ar>
2023-06-12 18:35:13 -0300

    add anda bien

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -22,21 +22,29 @@ let productsController = {
       },
 
       add: function(req,res){
+          if (req.session.user && req.session.user.id) {
+              // return res.send(String(req.session.user.id));
+            // } else {
+             //  return res.send("User id is not defined");
+             }
+             
+          //return res.send(String(req.session.user.id))
           return res.render('product-add')
       },
 
      submit: function(req,res){
        //   let form = req.params;
           let product = {
-               id_usuario: req.session.user.id,
-               nombreproducto: req.session.user.nombreproducto,
-               descripcionproducto: req.session.user.descripcionproducto,
+          
+               id_usuario: String(req.session.user.id),
+               nombreproducto: req.body.nombreproducto,
+               descripcionproducto: req.body.descripcionproducto,
            //    detalle: form.detalle
           }
            //return res.send(product)
           db.Products.create(product)
            .then(function(productoCreado){
-                return res.redirect('/', 'Producto añadido', { datosUsuario: productoCreado.usuario});
+                return res.redirect('/');
            })
            .catch(function(error){
                 console.log(error);
